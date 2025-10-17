1aa500a976f52fda839dc0fe0ac67a588c280c91
Wilson Heres <wheres67@gmail.com>
2025-10-17 15:38:26 -0400

    Implement initial workout handler and routing functionality

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/hereswilson/femProject/internal/app"
+	"github.com/hereswilson/femProject/internal/routes"
+)
+
+func main() {
+
+	var port int
+	flag.IntVar(&port, "port", 8080, "Port to run the web server on")
+	flag.Parse()
+
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+
+	r := routes.SetupRoutes(app)
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      r,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	app.Logger.Printf("Application started successfully on port %d\n", port)
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatalf("Could not start server: %v\n", err)
+	}
+}
