0f0fa98e1343f26f9f14a851136c9b0cd5403b13
Wilson Heres <wheres67@gmail.com>
2025-10-18 09:17:01 -0400

    Refactor database connection handling and add Docker Compose configuration for PostgreSQL

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,26 +1,36 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/hereswilson/femProject/internal/api"
+	"github.com/hereswilson/femProject/internal/store"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
+
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	workoutHandler := api.NewWorkoutHandler()
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}
 	return app, nil
 }
